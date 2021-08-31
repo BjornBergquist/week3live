3c6844bce717c21ae4acfa6a2ea362d018c1a38e
Björn Bergquist <73097586+BjornBergquist@users.noreply.github.com>
2021-08-31 12:12:51 +0200

    Added useState and useEffect-hooks.

--- src/views/storeview/StoreView.js
+++ src/views/storeview/StoreView.js
@@ -1,9 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 export const StoreView = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    alert("Hej dotNet20D!");
+    return () => {
+      alert("Hejdå!");
+    };
+  }, []);
+
   return (
     <div>
-      <h1>This is the store view</h1>
+      <h1>Our Amazing Product</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <span>{count}</span>
+      <button
+        onClick={() => {
+          setCount(count > 0 ? count - 1 : 0);
+        }}
+      >
+        -
+      </button>
     </div>
   );
 };
