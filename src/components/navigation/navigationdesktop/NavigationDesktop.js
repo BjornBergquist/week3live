a71bea4fc57db748b0c828fd41742fdd6e946dc3
Björn Bergquist <73097586+BjornBergquist@users.noreply.github.com>
2021-08-31 10:31:24 +0200

    Lecture 4

--- src/components/navigation/navigationdesktop/NavigationDesktop.js
+++ src/components/navigation/navigationdesktop/NavigationDesktop.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
+import RoutingPath from "../../../routes/RoutingPath";
 
 export const NavigationDesktop = () => {
   const history = useHistory();
   return (
     <nav>
-      <button onClick={() => history.push("/")}> Home </button>
-      <button onClick={() => history.push("/signin")}> Sign in </button>
+      <button onClick={() => history.push(RoutingPath.homeView)}>Home</button>
+      <button onClick={() => history.push(RoutingPath.storeView)}>Shop</button>
+      <button onClick={() => history.push(RoutingPath.signInView)}>
+        Sign in
+      </button>
+      {/* 
+      <Link to={RoutingPath.homeView}>Home</Link>
+      <Link to={RoutingPath.storeView}>Shop</Link>
+      <Link to={RoutingPath.signInView}>SignIn</Link> */}
     </nav>
   );
 };
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,14 +2,18 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { HomeView } from "../views/homeview/HomeView";
 import { SignInView } from "../views/signinview/SignInView";
+import { StoreView } from "../views/storeview/StoreView";
+import RoutingPath from "./RoutingPath";
 
 export const Routes = ({ children }) => {
   return (
     <BrowserRouter basename="/Week3Live">
       {children}
       <Switch>
-        <Route exact path="/signin" component={SignInView} />
-        <Route path="/" component={HomeView}></Route>
+        <Route exact path={RoutingPath.signInView} component={SignInView} />
+        <Route exact path={RoutingPath.storeView} component={StoreView} />
+        <Route path={RoutingPath.homeView} component={HomeView} />
+        {/*<Route path={RoutingPath.fourOFourView} component={FourOFourView} />*/}
       </Switch>
     </BrowserRouter>
   );
