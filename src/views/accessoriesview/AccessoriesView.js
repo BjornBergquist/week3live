e32909f323da5f904e240ffe3e58ace0ebf65984
Björn Bergquist <73097586+BjornBergquist@users.noreply.github.com>
2021-09-15 10:23:14 +0200

    Added a spinner.

--- src/views/accessoriesview/AccessoriesView.js
+++ src/views/accessoriesview/AccessoriesView.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import axios from "axios";
+import spinner from "../../shared/img/spinner.gif";
+import "./AccessoriesView.css";
 
 export const AccessoriesView = () => {
   const [serverResponse, setServerResponse] = useState();
-  const API_URL = "https://pokeapi.co/api/v2/pokemon/1";
+  const [count, setCount] = useState(1);
+  const API_URL = "https://pokeapi.co/api/v2/pokemon/" + count; // = `https://pokeapi.co/api/v2/pokemon/${counter}`;
 
   const fetchData = async () => {
     try {
       const response = await axios.get(API_URL);
       setServerResponse(response);
+      setCount(count + 1);
     } catch (error) {
       alert("Error retrieving data from server: " + error);
     }
@@ -16,9 +20,11 @@ export const AccessoriesView = () => {
 
   const showLoadingIfNoAPICall = () => {
     if (serverResponse) {
-      return serverResponse.data?.name;
+      return <h2>{serverResponse.data?.name}</h2>;
     } else {
-      return "Loading...";
+      return (
+        <img className="accessories__spinner" src={spinner} alt="Spinner" />
+      );
     }
   };
 
@@ -26,7 +32,7 @@ export const AccessoriesView = () => {
     <main>
       <section>
         <h1>This is the Accessories View</h1>
-        <h2>{showLoadingIfNoAPICall()}</h2>
+        {showLoadingIfNoAPICall()}
         <button onClick={() => fetchData()}>Make API call</button>
         <br />
         <img
